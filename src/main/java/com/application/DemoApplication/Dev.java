1544662c89fd58120ff42738e7bd9b7db554e806
AryenMukundam <aryenmukundam@gmail.com>
2026-01-15 23:38:35 +0530

    Notes Update

---
+++ src/main/java/com/application/DemoApplication/Dev.java
@@ -0,0 +1,11 @@
+package com.application.DemoApplication;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build(){
+        System.out.println("Building a Project");
+    }
+}
