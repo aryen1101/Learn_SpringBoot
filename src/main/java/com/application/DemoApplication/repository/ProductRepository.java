7fde51355c34e3a6e9a17c4d13486763a1b6d7c9
AryenMukundam <aryenmukundam@gmail.com>
2026-01-19 00:00:45 +0530

    Notes Updated

--- src/main/java/com/application/DemoApplication/repository/ProductRepository.java
+++ src/main/java/com/application/DemoApplication/repository/ProductRepository.java
@@ -4,9 +4,11 @@ import com.application.DemoApplication.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-
+    public List<Product> findByBrand(String brand);
 
 }
